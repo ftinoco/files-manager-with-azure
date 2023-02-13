06a1a373c8f63ad6b74174be895c951b4a018e46
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2023-02-12 22:59:39 -0600

    Creating an example how to download file from azure blob storage

--- FilesManagerWithAzure.APP/Controllers/HomeController.cs
+++ FilesManagerWithAzure.APP/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using FilesManagerWithAzure.APP.Models;
+using FilesManagerWithAzure.Core.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,15 +10,23 @@ namespace FilesManagerWithAzure.APP.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBlobService _blobService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IBlobService blobService)
         {
             _logger = logger;
+            _blobService = blobService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetImage()
+        {
+            var result = await _blobService.GetBlobByName("Screenshot_2023-01-23-06-23-36-352_com.yzbdz.shuma.jpg");
+            return File(result.Blob.ToArray(), result.ContentType);
         }
 
         public IActionResult Privacy()
